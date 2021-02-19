f26478f59f600891099088ebbc5e034b6f811009
Topov Vladislav <vladtopov2001@gmail.com>
2021-02-19 16:49:32 +0200

    TodoService + tests finished

--- src/main/java/com/topov/todo/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.topov.todo.exception;
-
-import lombok.Getter;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String s) {
-        super(s);
-    }
-}
